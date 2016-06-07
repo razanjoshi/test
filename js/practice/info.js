dd34f950fa24003bb4d96160d209a30ef0e17e09
bibhutiregmi <bibhutiregmi@gmail.com>
2016-06-07 16:37:42 +0545

    Form validation

--- js/practice/info.js
+++ js/practice/info.js
@@ -1,18 +1,22 @@
 'use strict'
-	function validateForm() {
-		debugger;
-    var x = document.forms["myForm"]["fname"].value;
-     var y = document.forms["myForm"]["lname"].value;
-     var z= document.forms["myForm"]["phone"].value;
-    if (x == null || x == "" || y == null || y == " ") {
-        alert("Both name must be filled out");
-        return false;
-    }
-    
-    if (z.toString().length  !=10){
-    	alert("numbers not equal to 10 digit");
-    	return false;
-    }
-}
+	function IsEmpty(){ 
 
-document.getElementById('submit').addEventListener('click', validateForm);
+		let first = document.getElementById('fname');
+		let lastname = document.getElementById('lname');
+		let phone = document.getElementById('phone');
+		debugger
+	if(first.value == "" || lastname.value == "")
+	{
+		alert("empty");
+	}
+	  
+
+	if(phone.value.length!=10)
+	{
+		alert("number not equal to 10");
+	}
+	   
+	
+	}
+
+	document.getElementById('submit').addEventListener("click",IsEmpty);
